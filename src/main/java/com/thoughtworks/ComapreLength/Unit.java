c2c56a60a8558eb8b4b62b37731e77444e88e555
sangharshasureshranpise <sangharsharanpise408@gmail.com>
2019-09-29 09:20:35 +0530

    [Sangharsha|Dipali] Refactor. conversion to cm

--- src/main/java/com/thoughtworks/ComapreLength/Unit.java
+++ src/main/java/com/thoughtworks/ComapreLength/Unit.java
@@ -1,16 +1,18 @@
 package com.thoughtworks.ComapreLength;
 
 public enum Unit {
-    Feet(12),
-    Inch(1);
+    Feet(12, 2.54),
+    Inch(1, 2.54);
 
     private double conversionFactor;
+    private double conversionToCM;
 
-    Unit(final double conversionFactor) {
+    Unit(final double conversionFactor, final double conversionToCM) {
         this.conversionFactor = conversionFactor;
+        this.conversionToCM = conversionToCM;
     }
 
     double convertToBase(double value) {
-        return conversionFactor * value;
+        return conversionFactor * value * conversionToCM;
     }
 }
